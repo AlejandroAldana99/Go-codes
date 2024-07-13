b639374fe39c7b411a2966074cecb7a55fd7dabb
AlejandroAldana99 <baker-plan-b@hotmail.com>
2024-07-12 21:02:09 -0600

    New stuff

--- Simple-Example/main.go
+++ Simple-Example/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 type User struct {
-	Name    *string
-	Age     int
-	Address AddressInfo
+	Name     *string
+	Age      int
+	Address  AddressInfo
+	IsActive bool
 }
 
 type AddressInfo struct {
@@ -15,7 +17,7 @@ type AddressInfo struct {
 }
 
 func main() {
-	println("Hello world")
+	// println("Hello world")
 
 	// var intExample int = 1
 	// var doubleExample float32 = 1.0
@@ -24,15 +26,47 @@ func main() {
 
 	// name := true
 
+	linstOfNumbers := []int{} // make([]int,2) // [1,2]
+	listUser := []User{}
+
 	name := ""
 	userInfo := User{
 		Address: AddressInfo{
 			Street: "Nada",
 		},
+		IsActive: false,
 	}
 
 	userInfo.Name = &name
+	listUser = append(listUser, userInfo)
 
-	fmt.Println(userInfo)
-	println(fmt.Sprintf("Name: %s, Address: %s", userInfo.Name, userInfo.Address.Street))
+	// fmt.Println(userInfo)
+	// println(fmt.Sprintf("Name: %v, Address: %s", userInfo.Name, userInfo.Address.Street))
+
+	// println(fmt.Sprintf("Is active: %v", isActiveValidator(userInfo.IsActive)))
+
+	maxNumber, err := validMaxNumber(linstOfNumbers)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	println(maxNumber)
+}
+
+func isActiveValidator(isActive bool) bool {
+	return isActive
+}
+
+func validMaxNumber(arrayInts []int) (int, error) {
+	if len(arrayInts) <= 0 {
+		return 0, errors.New("Invalid number")
+	}
+
+	number := 0
+	for i := range arrayInts {
+		number = arrayInts[i]
+	}
+
+	return number, nil
 }
